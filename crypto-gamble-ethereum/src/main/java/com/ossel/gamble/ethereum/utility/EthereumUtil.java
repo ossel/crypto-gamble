d4354096aea0856e6de90e52b62081cf6776f7cc
Dany <dany.brossel@hotmail.com>
2018-01-26 12:47:10 +0100

    ...

---
+++ crypto-gamble-ethereum/src/main/java/com/ossel/gamble/ethereum/utility/EthereumUtil.java
@@ -0,0 +1,35 @@
+package com.ossel.gamble.ethereum.utility;
+
+import java.math.BigInteger;
+
+public class EthereumUtil {
+
+
+    public static BigInteger uint(String hexHash) {
+        return new BigInteger(hexStringToByteArray(hexHash));
+    }
+
+
+    private final static char[] hexArray = "0123456789ABCDEF".toCharArray();
+
+    private static String bytesToHex(byte[] bytes) {
+        char[] hexChars = new char[bytes.length * 2];
+        for (int j = 0; j < bytes.length; j++) {
+            int v = bytes[j] & 0xFF;
+            hexChars[j * 2] = hexArray[v >>> 4];
+            hexChars[j * 2 + 1] = hexArray[v & 0x0F];
+        }
+        return new String(hexChars);
+    }
+
+    private static byte[] hexStringToByteArray(String s) {
+        int len = s.length();
+        byte[] data = new byte[len / 2];
+        for (int i = 0; i < len; i += 2) {
+            data[i / 2] = (byte) ((Character.digit(s.charAt(i), 16) << 4)
+                    + Character.digit(s.charAt(i + 1), 16));
+        }
+        return data;
+    }
+
+}
--- crypto-gamble-ethereum/src/test/java/com/ossel/gamble/ethereum/misc/Misc.java
+++ crypto-gamble-ethereum/src/test/java/com/ossel/gamble/ethereum/misc/Misc.java
@@ -3,12 +3,26 @@ package com.ossel.gamble.ethereum.misc;
 import static org.junit.Assert.assertEquals;
 import java.math.BigInteger;
 import org.junit.Test;
+import com.ossel.gamble.ethereum.utility.EthereumUtil;
 
 public class Misc {
 
     @Test
     public void testNullBlockhash() throws Exception {
         assertEquals(0, new BigInteger("0000000000000000000000").intValue());
+    }
+
+    @Test
+    public void testHexHashToUInt() throws Exception {
+        assertEquals(
+                "43010280667384004654441207986232896993269182950646931792897788114357618285314",
+                EthereumUtil
+                        .uint("5F16F4C7F149AC4F9510D9CF8CF384038AD348B3BCDC01915F95DE12DF9D1B02")
+                        .toString());
+        assertEquals(4,
+                EthereumUtil
+                        .uint("5F16F4C7F149AC4F9510D9CF8CF384038AD348B3BCDC01915F95DE12DF9D1B02")
+                        .mod(BigInteger.TEN).intValue());
     }
 
 
